72357ae0f2175ec5fd957f9bb1db897776d47a98
Subham Thapa <subhamits4.2@gmail.com>
2026-05-21 16:00:24 +0545

    shuffle the cards, clean up function and creating new cardset - shuffle cards - create a new function for generating, shuffling and assigning faceup   to the cards - after shuffling make the top 10 cards as faceup

--- src/App.svelte
+++ src/App.svelte
@@ -4,40 +4,56 @@
     import { ranks, suits, type CardType } from "./lib/types";
 
     let cards: CardType[] = [];
-
+    const MAX_FACE_UP = 10;
     function shuffle(cards: CardType[]) {
         const arr = [...cards];
+        let upCount = 0;
         for (let i = arr.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [arr[i], arr[j]] = [arr[j], arr[i]];
         }
         return arr;
     }
-    function handleShuffle() {
+    function faceUp(cards: CardType[]) {
+        const arr = [...cards];
+        let upCount = 0;
+        for (let i = 0; i < MAX_FACE_UP; i++) {
+            arr[i].faceUp = true;
+        }
+        return arr;
+    }
+    function newCards() {
+        generateCard();
         cards = shuffle(cards);
+        cards = faceUp(cards);
     }
     function generateCard() {
+        cards = [];
         let count = 0;
-        for (let i = 0; i < ranks.length; i++) {
-            for (let j = 0; j < suits.length; j++) {
-                cards.push({
-                    id: count.toString(),
-                    faceUp: count < 10 ? true : false,
-                    rank: ranks[i],
-                    suit: suits[j],
-                });
-                count++;
-            }
-        }
-        cards = cards;
+
+        const newCards: CardType[] = suits.flatMap((suit) =>
+            ranks.map((rank) => ({
+                id: (count++).toString(),
+                rank,
+                suit,
+                faceUp: false,
+            })),
+        );
+
+        cards = shuffle(newCards);
     }
     onMount(() => {
-        generateCard();
+        newCards();
     });
 </script>
 
 <div class="bg-blue-100 w-auto">
-    <button on:click={handleShuffle}> Shuffle </button>
+    <button
+        class="bg-green-400 px-5 hover:bg-green-300 hover:cursor-pointer m-2"
+        on:click={newCards}
+    >
+        Shuffle
+    </button>
     <div class="grid grid-cols-13 gap-2">
         {#each cards as card}
             <Card {card} />
